26970dfaca63d2e5c3f503bbffffb91b83739388
Eugene Essun Oliveros <jinyuzprodigy@gmail.com>
2017-11-06 23:12:13 +0800

    Update version to 0.1.3

--- pytuneteller/version.py
+++ pytuneteller/version.py
@@ -5,7 +5,7 @@
 
 MAJOR_VERSION = "0"
 MINOR_VERSION = "1"
-MICRO_VERSION = "3rc2"
+MICRO_VERSION = "3"
 
 def get_version():
     return "{major}.{minor}.{micro}".format(major=MAJOR_VERSION, minor=MINOR_VERSION, micro=MICRO_VERSION)
