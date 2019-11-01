09bde472d10731eb27303453b42f2b390a4ce234
Eugene Oliveros <eevoliveros@gmail.com>
2019-11-02 07:21:27 +0800

    Update author and version

--- pytuneteller/__init__.py
+++ pytuneteller/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for Pytuneteller."""
 
-__author__ = """Eugene Essun Oliveros"""
-__email__ = 'jinyuzprodigy@gmail.com'
-__version__ = '0.1.1'
+__author__ = """Eugene Oliveros"""
+__email__ = 'eevoliveros@gmail.com'
+__version__ = '0.2.0'
 
 from .pytuneteller import *
