83d1f03da02c494856a7a0347ae3547c7058404d
Eugene Oliveros <eevoliveros@gmail.com>
2019-11-02 21:47:58 +0800

    Update package version to 0.2.0

--- pytuneteller/version.py
+++ pytuneteller/version.py
@@ -4,8 +4,9 @@
 """
 
 MAJOR_VERSION = "0"
-MINOR_VERSION = "1"
-MICRO_VERSION = "3"
+MINOR_VERSION = "2"
+MICRO_VERSION = "0"
+
 
 def get_version():
     return "{major}.{minor}.{micro}".format(major=MAJOR_VERSION, minor=MINOR_VERSION, micro=MICRO_VERSION)
